efb4b44886146f44e8a0417b1f439c267f5d9db4
MD. MOHIBUR RAHMAN <mrpmohiburrahman@gmail.com>
2022-01-02 18:10:37 +0600

    implement Modal with bottom spacing

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+import Modal from "./react-native-modal/src/index";
+
+const BOTTOM_SPACING = 100; // <-- change here
 
 export default function App() {
+  const [isModalVisible, setModalVisible] = useState(false);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "flex-end",
+        alignItems: "center",
+      }}>
+      <Button
+        onPress={() => setModalVisible(!isModalVisible)}
+        title="show modal"
+      />
+      <Modal
+        testID={"modal"}
+        isVisible={isModalVisible}
+        style={{ justifyContent: "flex-end", margin: 0 }}
+        bottomSpacing={BOTTOM_SPACING}>
+        <View style={styles.card}>
+          <Text>Place your custom view inside BottomSheet</Text>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -13,8 +34,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  card: {
+    marginBottom: BOTTOM_SPACING,
+    backgroundColor: "#fff",
+    height: 250,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
